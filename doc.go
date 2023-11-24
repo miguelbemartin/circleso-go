5909189e765e23e5ea36d7058b0ff5dc9d3f4133
Miguel Martin <miguelbemartin@gmail.com>
2023-11-24 21:07:34 +0100

    Init go modules

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Package circle provides an API Client for working with the v1 of the Circle.so API.
+//
+// See https://api.circle.so/ for full documentation of the API which this
+// package makes requests to.
+package circle
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/miguelbemartin/circleso-go
+
+go 1.21.3
